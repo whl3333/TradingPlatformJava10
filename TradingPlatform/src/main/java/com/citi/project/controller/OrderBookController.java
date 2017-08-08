46f88c14a5245c8fdd702abc0e3b0e821fb83b9a
Better <429864974@qq.com>
2017-08-08 14:46:32 +0800

    可访问页面

--- TradingPlatform/src/main/java/com/citi/project/controller/OrderBookController.java
+++ TradingPlatform/src/main/java/com/citi/project/controller/OrderBookController.java
@@ -6,25 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.citi.project.entities.OrderBook;
 import com.citi.project.service.OrderBookService;
 
 
 @RestController
-@RequestMapping("/orderbooks")
+
 public class OrderBookController
 {
     @Autowired
    private OrderBookService orderBookService;
 
-    @RequestMapping(method = RequestMethod.GET)
+  
+    @RequestMapping("/orderbooks")
    public List<OrderBook> getAll(){
-//       List<OrderBook> list =  orderBookService.find();
-//       for(int i=0; i<list.size (); i++){
-//           list.get (i).setOrderID (0);
-//       }
-//       return list;
-       return orderBookService.find();
+       List<OrderBook> list =  orderBookService.find();
+       for(int i=0; i<list.size (); i++){
+           list.get (i).setOrderID (0);
+       }
+       return list;
    }
+    
+    @RequestMapping("/index")
+   public ModelAndView  index(){
+       return new ModelAndView("index");
+   }
+    
 }
